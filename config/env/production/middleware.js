4fe80166c99795de769c768e90b7d2f56c44c6a6
nomanmohammadofficial <nomanmohammadofficial@gmail.com>
2024-11-17 21:55:02 +0530

    Update CORS configuration to allow Vite development server

--- config/env/production/middleware.js
+++ config/env/production/middleware.js
@@ -2,10 +2,25 @@ module.exports = ({ env }) => ({
   settings: {
     cors: {
       enabled: true,
-      origin: ['https://nomanmohammad.com', 'https://www.nomanmohammad.com', 'http://localhost:3000'],
+      origin: [
+        'https://nomanmohammad.com',
+        'https://www.nomanmohammad.com',
+        'http://localhost:3000',
+        'http://localhost:5173', 
+        'http://127.0.0.1:5173'  
+      ],
       credentials: true,
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
-      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      headers: [
+        'Content-Type',
+        'Authorization',
+        'Origin',
+        'Accept',
+        'Access-Control-Allow-Origin',
+        'Access-Control-Allow-Credentials',
+        'Access-Control-Allow-Headers',
+        'Access-Control-Allow-Methods'
+      ],
     },
   },
 });
